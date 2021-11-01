1b6c6e0bd1361f2268853dd326a5435c57a644e8
Lawrence Forooghian <lawrence@forooghian.com>
2021-11-01 10:23:59 -0300

    Change class name of all QuickSpec to XCTestCase

--- Sources/ably-cocoa-tests-linter/main.swift
+++ Sources/ably-cocoa-tests-linter/main.swift
@@ -1,34 +1,73 @@
 import SwiftSyntax
 import Foundation 
 
-print("Hello, world!")
+// rewrites subclasses of QuickSpec to XCTestCase
+class TransformQuickSpec: SyntaxRewriter {
+    override func visit(_ node: ClassDeclSyntax) -> DeclSyntax {
+        guard let inheritanceClause = node.inheritanceClause else {
+            // If it doesn't inherit from anything, pass it through untouched.
+            return DeclSyntax(node)
+        }
+        
+        print("Processing class \(node.identifier)")
+        
+        let inheritedTypeCollection = inheritanceClause.inheritedTypeCollection
+        
+        let quickSpecInheritedType = inheritedTypeCollection.first { inheritedType in
+            let typeName = inheritedType.typeName
+            let token = typeName.firstToken!
+                                    
+            // Not sure if this is the best way to check (can we keep going further syntactically or are we at a terminal?)
+            guard case .identifier(let inheritedFromName) = token.tokenKind else {
+                fatalError("I expected an identifier to be the token here")
+            }
+            
+            return inheritedFromName == "QuickSpec"
+        }
+        
+        guard let quickSpecInheritedType = quickSpecInheritedType else {
+            // If it's not a subclass of QuickSpec, pass it through untouched.
+            return DeclSyntax(node)
+        }
+        
+        precondition(inheritedTypeCollection.count == 1, "I’m only equipped to handle things that inherit from one thing")
+        
+        let typeName = quickSpecInheritedType.typeName
+        let token = typeName.firstToken!
 
-/// AddOneToIntegerLiterals will visit each token in the Syntax tree, and
-/// (if it is an integer literal token) add 1 to the integer and return the
-/// new integer literal token.
-class AddOneToIntegerLiterals: SyntaxRewriter {
-  override func visit(_ token: TokenSyntax) -> Syntax {
-    // Only transform integer literals.
-    guard case .integerLiteral(let text) = token.tokenKind else {
-      return Syntax(token)
+        let newToken = token.withKind(.identifier("XCTestCase")) // TODO how to create this? Seems weird that I need an existing token to create a new one – isn't there a "token builder"?
+                
+        let typeIdentifier = SimpleTypeIdentifierSyntax({ builder in builder.useName(newToken) })
+        
+        let newTypeName = TypeSyntax(typeIdentifier) // No idea if this is right
+        
+        // The editing interface is a bit of a mystery to me – ah, these builders
+        // I found out about "simple type identifier" by looking at the TypeSyntax initializer’s source code
+                
+        let newInheritedType = quickSpecInheritedType.withTypeName(newTypeName)
+        let newInheritedTypes = inheritedTypeCollection.replacing(childAt: 0, with: newInheritedType)
+        let newInheritanceClause = inheritanceClause.withInheritedTypeCollection(newInheritedTypes)
+        let newNode = node.withInheritanceClause(newInheritanceClause)
+        
+        return DeclSyntax(newNode)
     }
-
-    // Remove underscores from the original text.
-    let integerText = String(text.filter { ("0"..."9").contains($0) })
-
-    // Parse out the integer.
-    let int = Int(integerText)!
-
-    // Create a new integer literal token with `int + 1` as its text.
-    let newIntegerLiteralToken = token.withKind(.integerLiteral("\(int + 1)"))
-    
-    // Return the new integer literal.
-    return Syntax(newIntegerLiteralToken)
-  }
 }
 
-let file = CommandLine.arguments[1]
-let url = URL(fileURLWithPath: file)
-let sourceFile = try SyntaxParser.parse(url)
-let incremented = AddOneToIntegerLiterals().visit(sourceFile)
-print(incremented)
+let directory = CommandLine.arguments[1]
+let directoryContents = try FileManager.default.contentsOfDirectory(atPath: directory)
+let swiftFiles = directoryContents.filter { $0.hasSuffix(".swift") }
+
+try swiftFiles.forEach { fileName in
+    print("Processing file \(fileName)")
+    let url = URL(fileURLWithPath: directory).appendingPathComponent(fileName)
+    
+    let sourceFile = try SyntaxParser.parse(url)
+    let transformed = TransformQuickSpec().visit(sourceFile)
+    
+    try String(describing: transformed).write(to: url, atomically: true, encoding: .utf8)
+    
+    print(transformed)
+}
+
+// https://forums.swift.org/t/se-0275-allow-more-characters-like-whitespaces-and-punctuations-for-escaped-identifiers/32538/50 - what should we use as the new method names?
+// what should we do with the xit test cases?
