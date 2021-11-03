efde75871c0eefb9d7b69b593866ca740352c69c
Lawrence Forooghian <lawrence@forooghian.com>
2021-11-03 17:29:39 -0300

    extract it-handling

--- Sources/ably-cocoa-tests-linter/main.swift
+++ Sources/ably-cocoa-tests-linter/main.swift
@@ -49,9 +49,10 @@ class TransformQuickSpecSubclass {
     }
     
     private func transformClassMember(_ member: MemberDeclListItemSyntax) -> [MemberDeclListItemSyntax] {
-
+        
         guard let functionDecl = member.decl.as(FunctionDeclSyntax.self) else {
             // TODO we need to figure out what else might be here
+            print("passing through unhandled \(member)")
             return [member]
         }
         
@@ -68,7 +69,7 @@ class TransformQuickSpecSubclass {
             fatalError("Don't know how to handle class-level function \(functionDecl.identifier) in \(containingClassName)")
         }
     }
-        
+    
     private func transformSpecFunctionDeclarationIntoClassLevelDeclarations(_ specFunctionDeclaration: FunctionDeclSyntax) -> [MemberDeclListItemSyntax] {
         // I think we want to lift the whole body of spec() and pull it out to the class body, keeping the variable declarations etc (see e.g. RestClient's spec)
         
@@ -79,12 +80,12 @@ class TransformQuickSpecSubclass {
         guard let specFunctionBody = specFunctionDeclaration.body else {
             fatalError("Don’t know how to handle function declaration without a body")
         }
-
+        
         let memberDeclListItems = specFunctionBody.statements.compactMap { statement -> [MemberDeclListItemSyntax]? in
             // It's a load of CodeBlockItemSyntax, for the variable declarations, then the beforeEach / afterEach, then the describe
-
+            
             // TODO what if there's stuff that clashes?
-
+            
             if let variableDeclaration = VariableDeclSyntax(statement.item) {
                 // Variable declarations just get hoisted outside of spec()
                 
@@ -110,7 +111,7 @@ class TransformQuickSpecSubclass {
                 preconditionFailure("I don't know how to handle this thing")
             }
         }.flatMap { $0 }
-
+        
         return memberDeclListItems
     }
     
@@ -124,57 +125,59 @@ class TransformQuickSpecSubclass {
         
         switch (calledFunctionName) {
         case "it":
-            // `it` gets turned into a method
+            return [transformItFunctionCallInsideSpecIntoClassLevelDeclaration(functionCallExpr)]
             
-            precondition(functionCallExpr.argumentList.count == 1, "`it` should only take one argument")
-            
-            // OK, this is a ExprSyntax, how do I find out whether it's a string literal?
-            let argumentExpression = functionCallExpr.argumentList.first!.expression
-
-            guard let stringLiteralExpression = argumentExpression.as(StringLiteralExprSyntax.self) else {
-                preconditionFailure("Expected the one argument to `it` to be a string literal describing the test")
-            }
-            
-            precondition(stringLiteralExpression.segments.count == 1, "the argument to `it` I'm expecting to only have one segment")
-            
-            let firstSegment = stringLiteralExpression.segments.first!
-            // TODO is this okay? Wasn't sure how to keep drilling
-            let testDescription = firstSegment.firstToken!.text
-            
-            // Now we grab the trailing closure from the call to `it` and use that as the new test method's body
-            
-            guard let trailingClosure = functionCallExpr.trailingClosure else {
-                preconditionFailure("I expect a call to `it` to have a trailing closure")
-            }
-            
-            guard trailingClosure.signature == nil else {
-                preconditionFailure("I don't expect the trailing closure to have any signature")
-            }
-                        
-            let testFunctionDeclaration = SyntaxFactory.makeFunctionDecl(
-                attributes: nil,
-                modifiers: nil,
-                funcKeyword: SyntaxFactory.makeFuncKeyword().withTrailingTrivia(.spaces(1)),
-                identifier: SyntaxFactory.makeIdentifier(testDescription) /* TODO this needs sanitizing */,
-                genericParameterClause: nil,
-                signature: SyntaxFactory.makeFunctionSignature(input: SyntaxFactory.makeParameterClause(leftParen: SyntaxFactory.makeLeftParenToken(), parameterList: SyntaxFactory.makeBlankFunctionParameterList(), rightParen: SyntaxFactory.makeRightParenToken()), asyncOrReasyncKeyword: nil, throwsOrRethrowsKeyword: nil, output: nil),
-                genericWhereClause: nil,
-                body: SyntaxFactory.makeCodeBlock(leftBrace: trailingClosure.leftBrace.withLeadingTrivia(.spaces(1)), statements: trailingClosure.statements, rightBrace: trailingClosure.rightBrace
-                                                 )
-            ).withLeadingTrivia(functionCallExpr.leadingTrivia!).withTrailingTrivia(functionCallExpr.trailingTrivia!)
-            
-            return [
-                SyntaxFactory.makeMemberDeclListItem(
-                    decl: DeclSyntax(testFunctionDeclaration),
-                    semicolon: nil
-                )
-            ]
         default:
             print("TODO handle spec()-level `\(calledFunctionName)`")
             return []
         }
     }
     
+    private func transformItFunctionCallInsideSpecIntoClassLevelDeclaration(_ functionCallExpr: FunctionCallExprSyntax) -> MemberDeclListItemSyntax {
+        // `it` gets turned into a method
+        
+        precondition(functionCallExpr.argumentList.count == 1, "`it` should only take one argument")
+        
+        // OK, this is a ExprSyntax, how do I find out whether it's a string literal?
+        let argumentExpression = functionCallExpr.argumentList.first!.expression
+        
+        guard let stringLiteralExpression = argumentExpression.as(StringLiteralExprSyntax.self) else {
+            preconditionFailure("Expected the one argument to `it` to be a string literal describing the test")
+        }
+        
+        precondition(stringLiteralExpression.segments.count == 1, "the argument to `it` I'm expecting to only have one segment")
+        
+        let firstSegment = stringLiteralExpression.segments.first!
+        // TODO is this okay? Wasn't sure how to keep drilling
+        let testDescription = firstSegment.firstToken!.text
+        
+        // Now we grab the trailing closure from the call to `it` and use that as the new test method's body
+        
+        guard let trailingClosure = functionCallExpr.trailingClosure else {
+            preconditionFailure("I expect a call to `it` to have a trailing closure")
+        }
+        
+        guard trailingClosure.signature == nil else {
+            preconditionFailure("I don't expect the trailing closure to have any signature")
+        }
+        
+        let testFunctionDeclaration = SyntaxFactory.makeFunctionDecl(
+            attributes: nil,
+            modifiers: nil,
+            funcKeyword: SyntaxFactory.makeFuncKeyword().withTrailingTrivia(.spaces(1)),
+            identifier: SyntaxFactory.makeIdentifier(testDescription) /* TODO this needs sanitizing */,
+            genericParameterClause: nil,
+            signature: SyntaxFactory.makeFunctionSignature(input: SyntaxFactory.makeParameterClause(leftParen: SyntaxFactory.makeLeftParenToken(), parameterList: SyntaxFactory.makeBlankFunctionParameterList(), rightParen: SyntaxFactory.makeRightParenToken()), asyncOrReasyncKeyword: nil, throwsOrRethrowsKeyword: nil, output: nil),
+            genericWhereClause: nil,
+            body: SyntaxFactory.makeCodeBlock(leftBrace: trailingClosure.leftBrace.withLeadingTrivia(.spaces(1)), statements: trailingClosure.statements, rightBrace: trailingClosure.rightBrace
+                                             )
+        ).withLeadingTrivia(functionCallExpr.leadingTrivia!).withTrailingTrivia(functionCallExpr.trailingTrivia!)
+        
+        return SyntaxFactory.makeMemberDeclListItem(
+            decl: DeclSyntax(testFunctionDeclaration),
+            semicolon: nil
+        )
+    }
 }
 
 // Transforms subclasses of QuickSpec to XCTestCase
@@ -192,7 +195,7 @@ class TransformQuickSpec: SyntaxRewriter {
         let quickSpecInheritedType = inheritedTypeCollection.first { inheritedType in
             let typeName = inheritedType.typeName
             let token = typeName.firstToken!
-                                    
+            
             // Not sure if this is the best way to check (can we keep going further syntactically or are we at a terminal?)
             guard case .identifier(let inheritedFromName) = token.tokenKind else {
                 fatalError("I expected an identifier to be the token here")
@@ -208,19 +211,19 @@ class TransformQuickSpec: SyntaxRewriter {
         
         precondition(inheritedTypeCollection.count == 1, "I’m only equipped to handle things that inherit from one thing")
         
-//        let typeName = quickSpecInheritedType.typeName
-//        let token = typeName.firstToken!
-
+        //        let typeName = quickSpecInheritedType.typeName
+        //        let token = typeName.firstToken!
+        
         let newToken = SyntaxFactory.makeToken(.identifier("XCTestCase"), presence: .present)
-//        let newToken = token.withKind(.identifier("XCTestCase")) // I'm keeping this around in case it helps with getting whitespace right
-                
+        //        let newToken = token.withKind(.identifier("XCTestCase")) // I'm keeping this around in case it helps with getting whitespace right
+        
         let typeIdentifier = SimpleTypeIdentifierSyntax({ builder in builder.useName(newToken) })
         
         let newTypeName = TypeSyntax(typeIdentifier) // No idea if this is right
         
         // The editing interface is a bit of a mystery to me – ah, these builders
         // I found out about "simple type identifier" by looking at the TypeSyntax initializer’s source code
-                
+        
         let newInheritedType = quickSpecInheritedType.withTypeName(newTypeName)
         let newInheritedTypes = inheritedTypeCollection.replacing(childAt: 0, with: newInheritedType)
         let newInheritanceClause = inheritanceClause.withInheritedTypeCollection(newInheritedTypes)
