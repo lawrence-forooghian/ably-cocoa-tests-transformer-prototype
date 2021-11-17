bb652ad913b026a8982c913a79c8c3c03a772497
Lawrence Forooghian <lawrence@forooghian.com>
2021-11-17 10:06:22 -0300

    change to a command line arg

--- Package.swift
+++ Package.swift
@@ -12,13 +12,17 @@ let package = Package(
             url: "https://github.com/apple/swift-syntax.git",
             .exact("0.50500.0")
         ),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "ably-cocoa-tests-linter",
-            dependencies: ["SwiftSyntax"]
+            dependencies: [
+                "SwiftSyntax",
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
         ),
         .testTarget(
             name: "ably-cocoa-tests-linterTests",
